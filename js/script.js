0db50c1b47c89c5ad6eae54ef31c5119d056ae04
Oleksandr <veremiienkoov96@gmail.com>
2025-08-08 11:37:56 -0400

    start Project

---
+++ js/script.js
@@ -0,0 +1,19 @@
+window.addEventListener('load', windowLoad);
+const html = document.documentElement;
+
+function windowLoad() {
+  if (992 >= window.innerWidth) {
+    document.addEventListener('click', documentActions);
+  }
+  html.classList.add('loaded');
+}
+function documentActions(e) {
+  const targetElement = e.target;
+
+  if (targetElement.closest('.icon-menu')) {
+    html.classList.toggle('menu-open');
+  }
+  targetElement.closest('.menu__link') && html.classList.contains('menu-open')
+    ? html.classList.remove('menu-open')
+    : null;
+}
